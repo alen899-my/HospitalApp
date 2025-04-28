bacdf71f3a69001c3e301c04a7a2a898b3b728b0
alen899-my <alenjames899@gmail.com>
2025-04-28 17:48:49 +0530

    login setup

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
-
+import "../styles/loginStyles/AdminLog.css"
+import adminlg from "../assets/loginImages/adminlg.jpg"
 const AdminLogin = () => {
   return (
-    <div>AdminLogin</div>
+    <div className="ad_login_container">
+             <div className="left_image">
+               <img src={adminlg} alt="" />
+             </div>
+             <div className="adloginBox">
+               <div className="adloginTxt">
+                 <h1>Admin Login</h1>
+               </div>
+               <form  className="ad_login_form">
+                 <div className="adform_group">
+                   <label htmlFor="email">Email</label>
+                   <input type="email" id="email" placeholder='enter your email' required />
+                 </div>
+                 <div className="adform_group">
+                   <label htmlFor="password">password</label>
+                   <input type="password" id="password" placeholder='enter your password' required/>
+       
+                 </div>
+                 <div className="adform_group">
+                   <button type="submit" className='adlogin_button'>login</button>
+                 </div>
+               </form>
+             </div>
+           </div>
   )
 }
 
